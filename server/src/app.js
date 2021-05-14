880771da74371cf352264eecea92d9c740b3d739
Akhil Goyal <akhilgoyal@akhils-mbp.lan>
2021-05-14 14:57:00 -0400

    Configured package.json to serve the build from server-side.

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 
@@ -9,6 +10,11 @@ app.use(cors({
     origin: 'http://localhost:3000'
 }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(planetsRouter);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
